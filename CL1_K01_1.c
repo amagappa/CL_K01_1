e3cf026543f1c70ead4c2e86d962ae4dc90b2dc6
shintaro okuda <bcso1220241@gn.iwasaki.ac.jp>
2013-01-18 10:11:11 +0900

    更新1
    日本語を削除

--- CL1_K01_1.c
+++ CL1_K01_1.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
 
-/* ì¬Ò 1T2(3) ‰œ“cT‘¾˜Y */
-/* ‰Û‘è–¼ : ‰Û‘è01_1 */
 void main(void){
 	int sum;
 	sum = 15 + 37;
-	printf("15‚É37‚ğ‘«‚µ‚½’l‚Í%d‚Å‚·\n",sum);
+	printf("15 + 37 = %d\n",sum);
 }
